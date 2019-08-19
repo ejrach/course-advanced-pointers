8e051f8b3c2e63b2b0ff1c888f228e2cc1bbd859
ejrach <eric.rach@gmail.com>
2019-08-19 16:20:10 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # course-advanced-pointers
 The course covers advanced topics in C pointers
+
+
+
+# This repository also includes a lesson from:
+"Pointers and Memory"
+https://www.youtube.com/watch?time_continue=5&v=wxCxB2aNouA
+
---
+++ eclipse-workspace/c_pointers/src/main.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : c_pointers.c
+ Author      : EricR
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Additional pointer exercises
+ ============================================================================
+ */
+
+// https://www.youtube.com/watch?time_continue=5&v=wxCxB2aNouA
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
